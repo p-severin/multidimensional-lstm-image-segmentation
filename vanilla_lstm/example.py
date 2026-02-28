9ab5e69d48a852a9fc835561e6a51ae3727d308c
Patryk Seweryn <patryk.seweryn@gmail.com>
2026-02-28 09:39:01 +0100

    Refactor main execution block to encapsulate model training and evaluation logic

--- vanilla_lstm/example.py
+++ vanilla_lstm/example.py
@@ -28,23 +28,24 @@ def get_sequences(n_sequences, n_timesteps):
     return seqX, seqY
 
 
-n_timesteps = 10
+if __name__ == '__main__':
+    n_timesteps = 10
 
-model = Sequential()
-model.add(Bidirectional(LSTM(50, return_sequences=True), input_shape=(n_timesteps, 1)))
-model.add(TimeDistributed(Dense(1, activation='sigmoid')))
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
-model.summary()
+    model = Sequential()
+    model.add(Bidirectional(LSTM(50, return_sequences=True), input_shape=(n_timesteps, 1)))
+    model.add(TimeDistributed(Dense(1, activation='sigmoid')))
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
+    model.summary()
 
-X, y = get_sequences(50000, n_timesteps)
-model.fit(X, y, epochs=1, batch_size=10)
+    X, y = get_sequences(50000, n_timesteps)
+    model.fit(X, y, epochs=1, batch_size=10)
 
-X, y = get_sequences(100, n_timesteps)
-loss, acc = model.evaluate(X, y, verbose=0)
-print(f'Loss : {loss}, Accuracy: {acc * 100}')
+    X, y = get_sequences(100, n_timesteps)
+    loss, acc = model.evaluate(X, y, verbose=0)
+    print(f'Loss : {loss}, Accuracy: {acc * 100}')
 
-for _ in range(10):
-    X, y = get_sequences(1, n_timesteps)
-    y_pred = (model.predict(X, verbose=0) > 0.5).astype('int32')
-    exp, pred = y.reshape(n_timesteps), y_pred.reshape(n_timesteps)
-    print(f'y = {y}, y_pred = {y_pred}, correct = {np.array_equal(exp, pred)}')
+    for _ in range(10):
+        X, y = get_sequences(1, n_timesteps)
+        y_pred = (model.predict(X, verbose=0) > 0.5).astype('int32')
+        exp, pred = y.reshape(n_timesteps), y_pred.reshape(n_timesteps)
+        print(f'y = {y}, y_pred = {y_pred}, correct = {np.array_equal(exp, pred)}')
